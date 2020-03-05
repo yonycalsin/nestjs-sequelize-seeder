7b791666b27a89a3bff1f5516373df0f80942e58
Yoni Calsin <yonicb20@gmail.com>
2020-03-05 10:30:07 -0500

    config: ⚙️ Creaated configuration file for git-cz

---
+++ lib/seed.constants.ts
@@ -0,0 +1,7 @@
+/**
+ * Injection tokens
+ */
+
+export const SEEDER_SERVICE_TOKEN = Symbol('SEEDER_SERVICE');
+export const SEEDER_TOKEN = 'SEEDER_TOKEN';
+export const SEEDER_OPTIONS = 'SEEDER_OPTIONS';
