76bbb8ba880d5db4352b39373ea943ad4ac0942f
Yoni Calsin <yonicb20@gmail.com>
2020-03-05 10:30:50 -0500

    module: 📰 Created seeder module !

---
+++ lib/seed.module.ts
@@ -0,0 +1,48 @@
+import { Module, DynamicModule } from '@nestjs/common';
+import { SeederService } from './seed.service';
+import { SEEDER_SERVICE_TOKEN, SEEDER_OPTIONS } from './seed.constants';
+import { SeederModuleOptions } from './interfaces';
+import { mergeDefaults } from './utils/merge-defaults.util';
+import { createSeederProviders } from './seed.providers';
+import { SequelizeOptions } from 'sequelize-typescript';
+import { DEFAULT_CONNECTION_NAME } from '@nestjs/sequelize/dist/sequelize.constants';
+import { SeederItem } from './interfaces';
+
+@Module({
+   providers: [
+      {
+         provide: SeederService,
+         useExisting: SEEDER_SERVICE_TOKEN,
+      },
+   ],
+   exports: [SeederService],
+})
+export class SeederModule {
+   static forRoot(options: SeederModuleOptions = {}): DynamicModule {
+      options = mergeDefaults(options);
+      const providers = [
+         {
+            provide: SEEDER_OPTIONS,
+            useValue: options,
+         },
+         SeederService,
+      ];
+      return {
+         module: SeederModule,
+         providers: [...providers],
+         global: options.isGlobal,
+         exports: [...providers],
+      };
+   }
+   static forFeature(
+      seeds: SeederItem[] = [],
+      connection: SequelizeOptions | string = DEFAULT_CONNECTION_NAME,
+   ): DynamicModule {
+      const providers = createSeederProviders(seeds, connection);
+      return {
+         module: SeederModule,
+         providers: [SeederService, ...providers],
+         exports: [SeederService, ...providers],
+      };
+   }
+}
