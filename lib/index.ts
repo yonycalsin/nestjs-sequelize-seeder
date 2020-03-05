43c377175ae0e9a8b6fb9b9b98ef8ce4ee1b4ca7
Yoni Calsin <yonicb20@gmail.com>
2020-03-05 10:32:54 -0500

    feat: 🎸 Created main file does it export all functions, decorators, and interfaces !

---
+++ lib/index.ts
@@ -0,0 +1,11 @@
+/*!
+ * Nestjs Sequelize Seeder v1.0.0 (https://github.com/yoicalsin/nestjs-sequelize-seeder)
+ * Copyright 2020 The Nestjs Sequelize Seeder Authors
+ * Copyright 2020 Yoni Calsin.
+ * Licensed under MIT (https://github.com/yoicalsin/nestjs-sequelize-seeder/blob/master/LICENSE)
+ */
+
+export * from './seed.module';
+export * from './seed.service';
+export * from './interfaces';
+export * from './seed.decorator';
