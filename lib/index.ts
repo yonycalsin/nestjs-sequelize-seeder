0bfd9aa3d88996fbea4c41b1efb6cf88734a111b
Yoni Calsin <yonicb20@gmail.com>
2020-05-06 22:53:31 -0500

    refactor: 💡 Moved to here all the types for typescript !

--- lib/index.ts
+++ lib/index.ts
@@ -5,7 +5,41 @@
  * Licensed under MIT (https://github.com/yoicalsin/nestjs-sequelize-seeder/blob/master/LICENSE)
  */
 
+import { Model } from 'sequelize-typescript';
+import { DEFAULT_CONNECTION_NAME } from '@nestjs/sequelize/dist/sequelize.constants';
+
+// For global
+export interface More {
+   [key: string]: any;
+}
+
+// For module
+export interface SeederModuleOptions {
+   isGlobal?: boolean;
+   logging?: boolean;
+   disabled?: boolean;
+   connection?: string;
+}
+
+// For decorators
+export interface OnSeederInit<T = More> {
+   run(options: SeederModuleOptions): (T | More)[];
+   everyone?(item: More | T): More | T;
+}
+
+export interface SeederOptions {
+   model: typeof Model;
+   unique: string | string[];
+}
+
+// Default data
+export const defaultOptions: SeederModuleOptions = {
+   isGlobal: true,
+   logging: true,
+   disabled: false,
+   connection: DEFAULT_CONNECTION_NAME,
+};
+
 export * from './seed.module';
 export * from './seed.service';
-export * from './interfaces';
 export * from './seed.decorator';
