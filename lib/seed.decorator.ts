39cb0d006b95e4e6c3133fde3ad81efbd0c7c359
Yoni Calsin <yonicb20@gmail.com>
2020-05-06 22:52:34 -0500

    fix: 🐛 Removed types !

--- lib/seed.decorator.ts
+++ lib/seed.decorator.ts
@@ -1,26 +1,11 @@
-import { Model } from 'sequelize-typescript';
 import Merge from 'merge-options-default';
 import { seeder_token } from './seed.constants';
 import { __rest } from 'tslib';
-import { SeederModuleOptions } from './interfaces';
+import { SeederOptions } from '.';
 
 const defaultOptions = {
    unique: [],
 };
-
-interface More {
-   [key: string]: any;
-}
-
-export interface OnSeederInit<T = More> {
-   run(options: SeederModuleOptions): (T | More)[];
-   everyone?(item: More | T): More | T;
-}
-
-interface SeederOptions {
-   model: typeof Model;
-   unique: string | string[];
-}
 
 export function Seeder(options?: SeederOptions) {
    options = Merge(defaultOptions, options, {
